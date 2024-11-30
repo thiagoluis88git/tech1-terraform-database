ce24c7a0fb8e1fa5b22986783eb3b9c802fb8467
Thiago Luis <thiago.luis88@gmail.com>
2024-11-30 00:10:55 -0300

    fix: labrole

--- infra/variables.tf
+++ infra/variables.tf
@@ -15,7 +15,7 @@ variable "networking" {
     region          = "us-east-1"
     profile         = "fiap-local"
     vpc_name        = "fiap-vpc"
-    fiap_role       = "arn:aws:iam::533267412722:role/LabRole"
+    fiap_role       = "arn:aws:iam::199710252834:role/LabRole"
     azs             = ["us-east-1a", "us-east-1b"]
     public_subnets  = ["10.0.64.0/24", "10.0.96.0/24"]
     private_subnets = ["10.0.0.0/24", "10.0.32.0/24"]
